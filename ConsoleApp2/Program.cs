558d0cb2ea901179035dc0beabed21f7f8ebbbe1
Selma <126454382+SelmaMammedli@users.noreply.github.com>
2023-04-19 01:31:31 +0400

    last version

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleApp20.Models;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Category category = new Category("Antibyotik");
+            Medicine medicine = new Medicine("C", 100, 1000);
+
+            string input = "";
+            do
+            {
+                Console.WriteLine("Apteke xosh geldiniz");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1.Category Menu");
+                Console.WriteLine("2.Derman Menusu");
+                Console.WriteLine("0.Exit");
+                Console.Write("Secim edin :  ");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("secim edin");
+                        string input2 = Console.ReadLine();
+                        switch (input2)
+                        {
+                            case "1":
+                                category.Create();
+                                break;
+                            case "2":
+                                category.Delete();
+                                break;
+                            case"3":
+                                category.Update();
+                                break;
+                           
+                                
+                            default:
+
+                                break;
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("secim edin");
+                        string input3 = Console.ReadLine();
+
+                        switch (input3)
+                        {
+                            case "1":
+                                medicine.Create();
+                                break;
+                            case "2":
+                                medicine.Delete();
+                                break;
+                            case "3":
+                                medicine.Update();
+                                break;
+                            case "4":
+                                medicine.Sell();
+                                break;
+                            default:
+
+                                break;
+                        }
+                        break;
+                }
+            }
+            while (input != "0");
+        }
+    }
+}
